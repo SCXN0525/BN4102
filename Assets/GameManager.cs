6681bb0248c12a482ebc02e9892acd4c2c5e2ac8
jordanjinyh <e0544420@u.nus.edu>
2023-03-24 14:40:29 +0800

    added scoring system inside

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -11,10 +11,6 @@ public class GameManager : MonoBehaviour
 
     public GrumpyBee player;
 
-    public Text scoreText; //new code line 1
-
-    private int score; //new code line 2
-
     private void Awake()
     {
         Application.targetFrameRate = 69;
@@ -31,24 +27,13 @@ public class GameManager : MonoBehaviour
     }
     void Start() //start of new codes
     {
-        score = 0;
+
     }
 
     void Update()
     {
-        // Detect when the player earns points
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            AddPoints(1); // Add 1 point to the score
-        }
-    }
 
-    // Add points to the score and update the score text
-    void AddPoints(int pointsToAdd)
-    {
-        score += pointsToAdd;
-        scoreText.text = "Score: " + score.ToString();
-    } //end of new codes
+    }
 
     public void GameOver()
     {
