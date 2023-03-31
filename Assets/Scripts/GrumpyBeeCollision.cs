b16b80bbee48777988dd856a631bf186a3560bd6
jordanjinyh <e0544420@u.nus.edu>
2023-03-31 10:02:00 +0800

    added life system

--- Assets/Scripts/GrumpyBeeCollision.cs
+++ Assets/Scripts/GrumpyBeeCollision.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GrumpyBeeCollision : MonoBehaviour
 {
     private Color originalColor;
     private SpriteRenderer spriteRenderer;
+    public int hearts = 3;
+    public Image[] heartImages;
+    public GameObject gameOverMessage;
+
+    private bool hasCollidedWithBrick = false;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         originalColor = spriteRenderer.color;
+        UpdateHearts();
+        gameOverMessage.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -22,14 +30,48 @@ public class GrumpyBeeCollision : MonoBehaviour
         }
         else if (other.gameObject.tag == "brick")
         {
-            GetComponent<SpriteRenderer>().color = Color.red;
+            if (!hasCollidedWithBrick)
+            {
+                spriteRenderer.color = Color.red;
+                hearts--;
+                UpdateHearts();
+                hasCollidedWithBrick = true;
+                if (hearts == 0)
+                {
+                    GameOver();
+                }
+            }
         }
     }
+
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("brick"))
+        if (other.gameObject.CompareTag("brick") && hasCollidedWithBrick)
         {
             spriteRenderer.color = originalColor;
+            hasCollidedWithBrick = false;
         }
     }
+
+    void UpdateHearts()
+    {
+        for (int i = 0; i < heartImages.Length; i++)
+        {
+            if (i < hearts)
+            {
+                heartImages[i].enabled = true;
+            }
+            else
+            {
+                heartImages[i].enabled = false;
+            }
+        }
+    }
+
+    void GameOver()
+    {
+        gameOverMessage.SetActive(true);
+        Time.timeScale = 0;
+    }
 }
+
