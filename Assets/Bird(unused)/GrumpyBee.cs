fa415dcef8b904b2ed5c698d240c45caae936d84
SCXN0525 <sherylchoo2000@gmail.com>
2023-04-08 09:29:18 +0800

    Integrate arduino to Unity

--- Assets/Bird(unused)/GrumpyBee.cs
+++ Assets/Bird(unused)/GrumpyBee.cs
@@ -1,22 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO.Ports;
 
 public class GrumpyBee : MonoBehaviour
 {
+    SerialPort data_stream = new SerialPort("COM6", 115200); //Arduino is connected to COM6 with 115200 baud rate
+    int potentiometerdata = 0;
+    int potentiometerdata1;
+    public Rigidbody rb;
+
     // speed at which it is moving
     public float speed = 1.5f;
 
-    // Up and Down movement of the Bird using keypad - to be changed to syncing up with Arduino
+    //called before the first frame update
+    void Start()
+    {
+        data_stream.Open(); //initialise serial stream
+    }
+
+
     private void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        potentiometerdata1 = int.Parse(data_stream.ReadLine()); //read serial data
+        // Debug.Log(potentiometerdata1);
+
+        //track change in potentiometer data to move the bee
+
+        if (potentiometerdata1 > potentiometerdata)
         {
-            transform.position += Vector3.up * speed * Time.deltaTime;
+            transform.position += Vector3.down * (potentiometerdata1 - potentiometerdata) * speed * Time.deltaTime;
+            // Debug.Log("down" + potentiometerdata1 + ',' + potentiometerdata);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+
+        else if (potentiometerdata1 < potentiometerdata)
         {
-            transform.position += Vector3.down * speed * Time.deltaTime;
+            transform.position += Vector3.up * (potentiometerdata - potentiometerdata1) * speed * Time.deltaTime;
+            // Debug.Log("up" + (potentiometerdata1-potentiometerdata));
         }
+
+        potentiometerdata = potentiometerdata1;
+        // Debug.Log("new potentiometer data: " + potentiometerdata);
     }
 }
